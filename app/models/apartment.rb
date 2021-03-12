f3998ce82c6005a70e7df4a03cb644772d0c3c05
zhsongallen <60937247+zhsongallen@users.noreply.github.com>
2021-03-12 21:33:34 +0800

    Remove fuzzily

--- app/models/apartment.rb
+++ app/models/apartment.rb
@@ -1,5 +1,2 @@
-# The apartment model
 class Apartment < ActiveRecord::Base
-    fuzzily_searchable :name
-    fuzzily_searchable :address #Enable fuzzy search, MyStuff.find_by_fuzzy_name('Some Name', :limit => 10)
 end
