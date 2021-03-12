bfaebaab3bc5808f477571a7da2fa3998913932e
Rebuild <admin@rebuild.moe>
2021-03-12 21:59:03 +0800

    Add a helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def text_abstract(text)
+    shortened = text[...text.index(" ", 80)]
+    shortened << "..." if shortened.length != text.length
+    shortened
+  end
 end
