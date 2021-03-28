be067e951ff9eb67db99e9cf1a237924f2f70924
fangyiwen <fyw1990@gmail.com>
2021-03-28 00:44:22 -0400

    Fix RSpec

--- spec/sessions_spec.rb
+++ spec/sessions_spec.rb
@@ -4,18 +4,18 @@ require_relative '../app/controllers/sessions_controller'
 RSpec.describe SessionsController, :type => :controller do
   it 'create_session' do
     user = {
-      username: "test",
-      password: "test"
+      username: "test999",
+      password: "test999"
     }
     u = User.new(**user, password_confirmation: 'nomatch')
     u.email = "#{user[:username]}@columbia.edu"
     u.password_confirmation = user[:password]
     u.save
 
-    post :create_session, params: { user: { username: 'test', password: 'test' } }
+    post :create_session, params: { user: { username: 'test999', password: 'test999' } }
     expect(response).to redirect_to root_path
 
-    post :create_session, params: { user: { username: 'test', password: 'test2' } }
+    post :create_session, params: { user: { username: 'test999', password: 'test2' } }
     expect(response).to redirect_to login_path
 
     u.destroy
@@ -23,36 +23,34 @@ RSpec.describe SessionsController, :type => :controller do
 
   it 'create_user' do
     user = {
-      username: "test",
-      password: "test"
+      username: "test999",
+      password: "test999"
     }
     u = User.new(**user, password_confirmation: 'nomatch')
     u.email = "#{user[:username]}@columbia.edu"
     u.password_confirmation = user[:password]
     u.save
 
-    post :create_user, params: { user: { username: 'test', password: 'test', password_confirmation: 'test' } }
+    post :create_user, params: { user: { username: 'test999', password: 'test999', password_confirmation: 'test999' } }
     expect(response).to redirect_to register_path
     expect(response.inspect.to_s).to include "User with this username already exists."
 
-    User.destroy_all
+    u.destroy
 
-    post :create_user, params: { user: { username: 'test', password: 'test', password_confirmation: 'test' } }
+    post :create_user, params: { user: { username: 'test999', password: 'test999', password_confirmation: 'test999' } }
     expect(response).to redirect_to root_path
 
-    User.destroy_all
+    User.where(username: "test999").destroy_all
 
-    post :create_user, params: { user: { username: 'test', password: 'test', password_confirmation: 'test2' } }
+    post :create_user, params: { user: { username: 'test9999', password: 'test9999', password_confirmation: 'test2' } }
     expect(response).to redirect_to register_path
     expect(response.inspect.to_s).to include "Password does not match."
-
-    User.destroy_all
   end
 
   it 'delete_session' do
     user = {
-      username: "test",
-      password: "test"
+      username: "test999",
+      password: "test999"
     }
     u = User.new(**user, password_confirmation: 'nomatch')
     u.email = "#{user[:username]}@columbia.edu"
